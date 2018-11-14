4568409a2581f1501ee60e52d34e622987ef0ce6
wallandall <wallandall@gmail.com>
2018-11-14 18:49:41 +0100

    Handle oAuth

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const passport = require('passport');
 
 router.get('/google', passport.authenticate('google',
-  {scope: ['profile', 'email']}));
+{scope: ['profile', 'email']}));
+
+router.get('/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/dashboard');
+});
 
 
 
