2704253007ee5e9e52f67698556ea064d48bce73
wallandall <wallandall@gmail.com>
2018-11-15 22:22:19 +0100

    Added Side Navigation, footer, and auth helper

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
@@ -14,6 +15,7 @@ require('./config/passport')(passport);
 // Load Routes
 const index = require('./routes/index');
 const auth = require('./routes/auth');
+const stories = require('./routes/stories');
 
 // Load Keys
 const keys = require('./config/keys');
@@ -52,9 +54,13 @@ mongoose.connect(keys.mongoURI, {
     next();
   });
 
+  //Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
   // Use Routes
   app.use('/', index);
   app.use('/auth', auth);
+  app.use('/stories', stories);
 
 
   const port = process.env.PORT || 5000;
