68156019f87de6ad8f9c70e6427facb7fb7067cd
wallandall <wallandall@gmail.com>
2018-11-17 17:14:05 +0100

    Added comment functionality

--- routes/stories.js
+++ routes/stories.js
@@ -107,4 +107,25 @@ router.delete('/:id', (req, res) => {
     });
 });
 
+// Add Comment
+router.post('/comment/:id', (req, res) => {
+  Story.findOne({
+    _id: req.params.id
+  })
+  .then(story => {
+    const newComment = {
+      commentBody: req.body.commentBody,
+      commentUser: req.user.id
+    }
+
+    // Add to comments array
+    story.comments.unshift(newComment);
+
+    story.save()
+      .then(story => {
+        res.redirect(`/stories/show/${story.id}`);
+      });
+  });
+});
+
 module.exports = router;
